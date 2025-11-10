0d36246ac0edc49df462a39e0c4452ba6feac5fd
Karolina <k.sech@student.fontys.nl>
2025-11-10 20:46:55 +0100

    To do add task functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import './styles/App.scss'
+import { useState } from 'react'
 
 function App() { 
-  return <h1>TO DO</h1>
+
+  const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() { 
+    if (input.trim() === '') return; // prevent adding empty tasks
+    setTasks([...tasks, input]);
+    setInput(''); // clear input field after adding
+  }
+  
+  return (
+    <div className="todo-app">
+      <h1>To Do App</h1>
+
+      <div id="todo-container">
+        <div id="input-container">
+          <input 
+            type="text" 
+            value={input}   
+            onChange={(e) => setInput(e.target.value)}  // update state on typing
+            id="todo-input" placeholder="Add a new task..." />
+          <button id="add-button" onClick={addTask}>Add task</button>
+        </div>
+
+        <ul id="todo-list">
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App
